eaac6c95f6f7f45760316806c330515ca6446180
Luke Robinett <lukebootcamp@gmail.com>
2019-12-07 22:44:02 -0800

    Added newly created API key and app ID for nutrition API. Still working on finalizing nutrition API functionality.

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,18 @@
-const appID = "599eeff6";
-const apiKey = "9ba899b6cf470706c026e545e7e3e1b6";
-const maxHits = 12; // Change this to however many hits you want the page to display after a search
+// API credentials
+const recipeAppID = "599eeff6";
+const recipeApiKey = "9ba899b6cf470706c026e545e7e3e1b6";
+const nutritionAppID = "fd617fa3";
+const nutritionApiKey = "6e6f9be7510b48fac69160d4728c2166";
+
+// Change this to however many hits you want the page to display after a search
+const maxHits = 12;
 
 $(document).ready(function () {
 
     $("#previewCards").hide();
     $("#recipeNutritionCard").hide();
 
-    $("#searchButton").click(function(event) {
+    $("#searchButton").click(function (event) {
         event.preventDefault();
 
         var searchText = $("#searchField").val().trim();
@@ -26,7 +31,7 @@ $(document).ready(function () {
 });
 
 function getRecipes(searchText) {
-    var queryUrl = "https://api.edamam.com/search?q=" + searchText + "&app_id=" + appID + "&app_key=" + apiKey + "&from=0&to=" + maxHits;
+    var queryUrl = "https://api.edamam.com/search?q=" + searchText + "&app_id=" + recipeAppID + "&app_key=" + recipeApiKey + "&from=0&to=" + maxHits;
     $.ajax({
         url: queryUrl,
         method: "GET"
@@ -36,52 +41,61 @@ function getRecipes(searchText) {
         $("#previewCards").empty();
         for (var i = 0; i < response.hits.length; i++) {
             var card = $("<div>").addClass("card");
-                var blurDimImg = $("<div>").addClass("blurring dimmable image");
-                    var uiDimmer = $("<div>").addClass("ui dimmer");
-                        var content = $("<div>").addClass("content");
-                            var center = $("<div>").addClass("center");
-                                var seeRecipeButton = $("<div>").addClass("ui inverted button recipeButton").text("See Recipe").attr('id', 'recipeButton');
-                                var saveRecipeButton = $("<div>").addClass("ui inverted button").text("Save Recipe");
-                            center.append(seeRecipeButton, saveRecipeButton);
-                        content.append(center);
-                    uiDimmer.append(content);
-                blurDimImg.append(uiDimmer);
-                var img = $("<img>").attr("alt", response.hits[i].recipe.label).attr("src", response.hits[i].recipe.image);
-                blurDimImg.append(img);
+            var blurDimImg = $("<div>").addClass("blurring dimmable image");
+            var uiDimmer = $("<div>").addClass("ui dimmer");
+            var content = $("<div>").addClass("content");
+            var center = $("<div>").addClass("center");
+            var seeRecipeButton = $("<div>").addClass("ui inverted button recipeButton").text("See Recipe").attr('id', 'recipeButton');
+            var saveRecipeButton = $("<div>").addClass("ui inverted button").text("Save Recipe");
+            center.append(seeRecipeButton, saveRecipeButton);
+            content.append(center);
+            uiDimmer.append(content);
+            blurDimImg.append(uiDimmer);
+            var img = $("<img>").attr("alt", response.hits[i].recipe.label).attr("src", response.hits[i].recipe.image);
+            blurDimImg.append(img);
             card.append(blurDimImg);
             var content2 = $("<div>").addClass("content");
-                var header = $("<a>").addClass("recipe-title-link").text(response.hits[i].recipe.label).attr("href", response.hits[i].recipe.url);
-                    var meta = $("<div>").addClass("meta");
-                        var span = $("<span>").addClass("text").text("from " + response.hits[i].recipe.source);
-                    meta.append(span);
-                content2.append(header, meta);
+            var header = $("<a>").addClass("recipe-title-link").text(response.hits[i].recipe.label).attr("href", response.hits[i].recipe.url);
+            var meta = $("<div>").addClass("meta");
+            var span = $("<span>").addClass("text").text("from " + response.hits[i].recipe.source);
+            meta.append(span);
+            content2.append(header, meta);
             card.append(content2);
 
             $("#previewCards").append(card);
             $("#previewCards").show();
             $('.special.cards .image').dimmer({
-                on: 'hover'   
+                on: 'hover'
             });
-            
+
         }
-        
+
     });
-    
+
 }
 
-$("#recipeButton").click(function() {
+$("#recipeButton").click(function () {
     console.log("good job")
 });
 
 
 function getNutrition(ingredients) {
-    var queryUrl = "https://api.edamam.com/api/nutrition-data?app_id=" + appID + "&app_key=" + apiKey + "&ingr=1%20large%20apple";
+    var queryUrl = "http://api.edamam.com/api/nutrition-details?app_id=" + nutritionAppID + "&app_key=" + nutritionApiKey;
     $.ajax({
         url: queryUrl,
-        method: "POST",
+        type: "POST",
+        data: {
+            "title": "BLT",
+            "ingr": [
+                "white bread",
+                "bacon",
+                "lettuce",
+                "tomatoes",
+                "mayonnaise"
+            ]
+        },
+        contentType: "application/json"
     }).then(function (response) {
-       console.log(response);
+        console.log(response);
     });
-
-    
 }
